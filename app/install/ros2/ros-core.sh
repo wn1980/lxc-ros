b8eb99911370c78eebb75bc65b7e33748cafc9d9
wn1980 <waipotn@hotmail.com>
2022-03-31 05:48:12 +0700

    add ros2 install

---
+++ app/install/ros2/ros-core.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+#define ROS distro
+export ROS_DISTRO=galactic
+
+sudo apt update && sudo apt install -y \
+    locales \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+
+sudo apt install -y ros-${ROS_DISTRO}-ros-core
+
+sudo apt install -y \
+    python3-rosdep \
+    python3-colcon-common-extensions
+
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
+echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> ~/.bashrc
+
+sudo apt autoremove -y 
+sudo apt-get clean
---
+++ app/install/ros2/turtlebot2.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -e
+
+#define ROS distro
+ROS_DISTRO=galactic
+
+TURTLEBOT_WS=$HOME/dev_ws
+
+sudo apt-get install git -y
+
+if [ ! -d $TURTLEBOT_WS/src/kobuki_ros ]; then
+
+  mkdir -p $TURTLEBOT_WS/src
+
+  cd $TURTLEBOT_WS/src
+
+  git clone https://github.com/stonier/sophus.git -b release/1.2.x
+  git clone https://github.com/stonier/ecl_core.git -b release/1.2.x
+  git clone https://github.com/stonier/ecl_lite.git -b release/1.1.x
+  git clone https://github.com/stonier/ecl_tools.git -b release/1.0.x
+
+  git clone https://github.com/kobuki-base/kobuki_core.git
+  git clone https://github.com/kobuki-base/velocity_smoother.git
+  git clone https://github.com/kobuki-base/cmd_vel_mux.git
+  
+  git clone https://github.com/kobuki-base/kobuki_ros_interfaces.git
+
+   git clone https://github.com/kobuki-base/kobuki_ros.git
+
+  # Additionals
+  git clone https://github.com/YDLIDAR/ydlidar_ros2_driver.git
+  git clone https://github.com/wn1980/turtlebot2_ros2.git
+
+fi
+
+sudo apt-get update && sudo apt-get upgrade -y 
+
+# install ydlidar_sdk first
+cd ~
+git clone https://github.com/YDLIDAR/YDLidar-SDK.git 
+mkdir -p YDLidar-SDK/build
+cd YDLidar-SDK/build
+cmake ..
+sudo make install
+rm -rf ~/YDLidar-SDK
+
+# make and install
+cd $TURTLEBOT_WS
+
+sudo rosdep install -i --from-path src --rosdistro ${ROS_DISTRO} -y
+  
+source /opt/ros/${ROS_DISTRO}/setup.bash
+
+colcon build --symlink-install
+
+# Install additionals and clean
+sudo apt-get install  -y \
+  #ros-${ROS_DISTRO}-kobuki-core \
+  #ros-${ROS_DISTRO}-kobuki-ftdi \
+  #ros-${ROS_DISTRO}-kobuki-firmware \
+  man \
+  && sudo apt-get autoremove -y \
+  && sudo apt-get clean \
+  && sudo rm -rf /var/lib/apt/lists/*
