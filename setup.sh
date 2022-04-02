5f87d13237a720a754e4e38ae17a3ab6a5ffadfd
wn1980 <waipotn@hotmail.com>
2022-04-02 20:22:13 +0700

    update

--- setup.sh
+++ setup.sh
@@ -3,16 +3,17 @@
 set -e
 
 #lxc delete ros -f
+#lxc profile delete ports
+
 lxc launch ubuntu:20.04 ros
 lxc file push -r app ros/
 
-#lxc profile delete ports
 lxc profile create ports
 lxc profile edit ports < profiles/ports.yml
 lxc profile add ros ports
 
+lxc restart ros
+
 lxc exec ros bash /app/install/supervisor.sh
 lxc exec ros bash /app/install/ttyd.sh
 lxc exec ros bash /app/install/app-chown.sh
-
-lxc restart ros
